18bb9c4a829acbb32ce729f0e08b36ed47a70572
Philippe Gaultier <philippe.gaultier@ppro.com>
2021-03-12 09:55:38 +0100

    [wip]

--- ui.h
+++ ui.h
@@ -2,6 +2,17 @@
 #include "deps/termbox/src/termbox.h"
 #include "deps/termbox/src/utf8.c"
 
+typedef struct {
+  int max_width_cols[4];
+  int w, h, selected;
+  pipeline_t** pipelines;
+
+} table_t;
+
+static table_t table_init() {
+  table_t table = {.max_width_cols = {0, 9, 9, 0}};
+}
+
 static void ui_init() {
   tb_init();
   tb_set_cursor(TB_HIDE_CURSOR, TB_HIDE_CURSOR);
@@ -21,44 +32,6 @@ static void ui_blank_draw(int count, int* x, int y, u16 fg, u16 bg) {
     tb_change_cell(*x, y, 0, fg, bg);
     *x += 1;
   }
-}
-
-static void ui_pipelines_header_draw(int* x, int max_width_ref,
-                                     int max_width_status,
-                                     int max_width_created,
-                                     int max_width_updated) {
-  // REF
-  {
-    const char header[] = "REF";
-    ui_string_draw(header, LEN0(header), x, 0, TB_WHITE | TB_BOLD, TB_DEFAULT);
-    ui_blank_draw(2 + max_width_ref - LEN0(header), x, 0, TB_DEFAULT,
-                  TB_DEFAULT);
-  }
-
-  // Created
-  {
-    const char header[] = "CREATED";
-    ui_string_draw(header, LEN0(header), x, 0, TB_WHITE | TB_BOLD, TB_DEFAULT);
-    ui_blank_draw(2 + max_width_created - LEN0(header), x, 0, TB_DEFAULT,
-                  TB_DEFAULT);
-  }
-
-  // Status
-  {
-    const char header[] = "UPDATED";
-    ui_string_draw(header, LEN0(header), x, 0, TB_WHITE | TB_BOLD, TB_DEFAULT);
-    ui_blank_draw(2 + max_width_updated - LEN0(header), x, 0, TB_DEFAULT,
-                  TB_DEFAULT);
-  }
-
-  // Status
-  {
-    const char header[] = "STATUS";
-    ui_string_draw(header, LEN0(header), x, 0, TB_WHITE | TB_BOLD, TB_DEFAULT);
-    ui_blank_draw(2 + max_width_status - LEN0(header), x, 0, TB_DEFAULT,
-                  TB_DEFAULT);
-  }
-  ui_blank_draw(tb_width() - *x, x, 0, TB_DEFAULT, TB_DEFAULT);
 }
 
 static int ui_iso_date_to_short_time(const sds date, const struct tm* now,
@@ -91,78 +64,107 @@ static int ui_iso_date_to_short_time(const sds date, const struct tm* now,
     return sprintf(res, "now");
 }
 
-static void ui_pipelines_draw(int* pipelines_count, int* pipeline_selected) {
-  int max_width_ref = 0, max_width_status = 0, max_width_created = 9,
-      max_width_updated = 9;
+static void table_header_draw(table_t* table) {
+  int col = 0, x = 0;
+  // Ref
   {
-    for (u64 i = 0; i < buf_size(projects); i++) {
-      const project_t* const project = &projects[i];
-
-      for (u64 j = 0; j < buf_size(project->pro_pipelines); j++) {
-        const pipeline_t* const pipeline = &project->pro_pipelines[j];
-
-        max_width_ref = MAX(max_width_ref, sdslen(pipeline->pip_vcs_ref));
-        max_width_status = MAX(max_width_status, sdslen(pipeline->pip_status));
-
-        *pipelines_count += 1;
-      }
-    }
+    const char header[] = "REF";
+    ui_string_draw(header, LEN0(header), &x, 0, TB_WHITE | TB_BOLD, TB_DEFAULT);
+    ui_blank_draw(2 + table->max_width_cols[col++] - LEN0(header), &x, 0,
+                  TB_DEFAULT, TB_DEFAULT);
   }
 
-  int y = 0, x = 0, k = 0;
-  ui_pipelines_header_draw(&x, max_width_ref, max_width_status,
-                           max_width_created, max_width_updated);
-  y++;
+  // Created
+  {
+    const char header[] = "CREATED";
+    ui_string_draw(header, LEN0(header), &x, 0, TB_WHITE | TB_BOLD, TB_DEFAULT);
+    ui_blank_draw(2 + table->max_width_cols[col++] - LEN0(header), &x, 0,
+                  TB_DEFAULT, TB_DEFAULT);
+  }
 
-  time_t now_epoch = time(NULL);
-  struct tm now;
-  gmtime_r(&now_epoch, &now);
+  // Status
+  {
+    const char header[] = "UPDATED";
+    ui_string_draw(header, LEN0(header), &x, 0, TB_WHITE | TB_BOLD, TB_DEFAULT);
+    ui_blank_draw(2 + table->max_width_cols[col++] - LEN0(header), &x, 0,
+                  TB_DEFAULT, TB_DEFAULT);
+  }
+
+  // Status
+  {
+    const char header[] = "STATUS";
+    ui_string_draw(header, LEN0(header), &x, 0, TB_WHITE | TB_BOLD, TB_DEFAULT);
+    ui_blank_draw(2 + table->max_width_cols[col++] - LEN0(header), &x, 0,
+                  TB_DEFAULT, TB_DEFAULT);
+  }
+  ui_blank_draw(tb_width() - x, &x, 0, TB_DEFAULT, TB_DEFAULT);
+}
+
+static void table_draw(table_t* table) {
+  buf_clear(table->pipelines);
 
   for (u64 i = 0; i < buf_size(projects); i++) {
     const project_t* const project = &projects[i];
 
     for (u64 j = 0; j < buf_size(project->pro_pipelines); j++) {
       const pipeline_t* const pipeline = &project->pro_pipelines[j];
-      x = 0;
 
-      int fg = TB_BLUE, bg = TB_DEFAULT;
-      if (*pipeline_selected == k) {
-        fg = TB_WHITE;
-        bg = TB_BLUE;
-      }
+      int col = 0;
+      table->max_width_cols[col] =
+          MAX(table->max_width_cols[col], sdslen(pipeline->pip_vcs_ref));
+      col++;
+      table->max_width_cols[col] =
+          MAX(table->max_width_cols[col], sdslen(pipeline->pip_status));
 
-      ui_string_draw(pipeline->pip_vcs_ref, sdslen(pipeline->pip_vcs_ref), &x,
-                     y, fg, bg);
-      ui_blank_draw(2 + max_width_ref - sdslen(pipeline->pip_vcs_ref), &x, y,
-                    fg, bg);
-
-      char res[10] = "";
-      int width =
-          ui_iso_date_to_short_time(pipeline->pip_created_at, &now, res);
-      ui_string_draw(res, width, &x, y, fg, bg);
-      ui_blank_draw(2 + LEN0(res) - width, &x, y, fg, bg);
-
-      memset(res, 0, LEN0(res));
-      width = ui_iso_date_to_short_time(pipeline->pip_updated_at, &now, res);
-      ui_string_draw(res, width, &x, y, fg, bg);
-      ui_blank_draw(2 + LEN0(res) - width, &x, y, fg, bg);
-
-      char status[40] = "";
-      memcpy(status, pipeline->pip_status,
-             MIN(LEN0(status), sdslen(pipeline->pip_status)));
-      ui_string_draw(status, max_width_status, &x, y, fg, bg);
-      ui_blank_draw(2, &x, y, fg, bg);
-
-      y++;
-      k++;
+      buf_push(table->pipelines, pipeline);
     }
+  }
+
+  table_header_draw(table);
+
+  time_t now_epoch = time(NULL);
+  struct tm now;
+  gmtime_r(&now_epoch, &now);
+
+  for (u64 y = 1; y <= buf_size(table->pipelines); y++) {
+    const pipeline_t* const pipeline = table->pipelines[y - 1];
+    int x = 0, col = 0;
+
+    int fg = TB_BLUE, bg = TB_DEFAULT;
+    if (table->selected == y - 1) {
+      fg = TB_WHITE;
+      bg = TB_BLUE;
+    }
+
+    ui_string_draw(pipeline->pip_vcs_ref, sdslen(pipeline->pip_vcs_ref), &x, y,
+                   fg, bg);
+    ui_blank_draw(
+        2 + table->max_width_cols[col++] - sdslen(pipeline->pip_vcs_ref), &x, y,
+        fg, bg);
+
+    char res[10] = "";
+    int width = ui_iso_date_to_short_time(pipeline->pip_created_at, &now, res);
+    ui_string_draw(res, width, &x, y, fg, bg);
+    ui_blank_draw(2 + table->max_width_cols[col++] - width, &x, y, fg, bg);
+
+    memset(res, 0, LEN0(res));
+    width = ui_iso_date_to_short_time(pipeline->pip_updated_at, &now, res);
+    ui_string_draw(res, width, &x, y, fg, bg);
+    ui_blank_draw(2 + table->max_width_cols[col++] - width, &x, y, fg, bg);
+
+    char status[40] = "";
+    memcpy(status, pipeline->pip_status,
+           MIN(LEN0(status), sdslen(pipeline->pip_status)));
+    ui_string_draw(status, table->max_width_cols[col++], &x, y, fg, bg);
+    ui_blank_draw(2, &x, y, fg, bg);
   }
 }
 
 static void ui_draw() {
-  int pipelines_count = 0, pipeline_selected = 0;
+  table_t table = table_init();
+
   tb_clear();
-  ui_pipelines_draw(&pipelines_count, &pipeline_selected);
+  table_draw(&table);
   tb_present();
 
   struct tb_event event;
@@ -170,21 +172,21 @@ static void ui_draw() {
     switch (event.type) {
       case TB_EVENT_RESIZE:
         tb_clear();
-        ui_pipelines_draw(&pipelines_count, &pipeline_selected);
+        table_draw(&table);
         tb_present();
         break;
 
       case TB_EVENT_KEY:
         if (event.key == TB_KEY_ARROW_DOWN &&
-            pipeline_selected < pipelines_count) {
-          pipeline_selected++;
+            table.selected < buf_size(table.pipelines)) {
+          table.selected++;
           tb_clear();
-          ui_pipelines_draw(&pipelines_count, &pipeline_selected);
+          table_draw(&table);
           tb_present();
-        } else if (event.key == TB_KEY_ARROW_UP && pipeline_selected > 0) {
-          pipeline_selected--;
+        } else if (event.key == TB_KEY_ARROW_UP && table.selected > 0) {
+          table.selected--;
           tb_clear();
-          ui_pipelines_draw(&pipelines_count, &pipeline_selected);
+          table_draw(&table);
           tb_present();
         } else if (event.key == TB_KEY_ESC || event.key == TB_KEY_CTRL_C ||
                    event.key == TB_KEY_CTRL_D) {
